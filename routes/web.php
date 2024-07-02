0f51a178288ddef8b1514e2bd4efbc06d941502f
sahil gupta <sahilgupta@founderandlightning.com>
2024-07-02 12:20:46 +0530

    return view

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('dashboard');
+    return view('login');
 })->name('home');
 
 Route::get('/api', function () {
